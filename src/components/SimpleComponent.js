91cb6991328ac0b803f65caca16d99340fb2ef3a
github username <github email address>
2019-06-26 16:41:11 -0400

    test passed

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,31 @@
-// Code SimpleComponent Here
+
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+
+  constructor () {
+    super();
+
+    this.state = {
+      mood: "happy"
+    }
+  }
+
+
+  render() {
+    console.log("inside render")
+    return (
+      <div onClick={this.handleClick}>{this.state.mood}</div>
+    );
+  }
+
+  handleClick = () =>{
+    // e.preventDefault();
+    this.setState({
+      mood: "sad"
+    })
+  }
+
+}
+
+export default SimpleComponent;

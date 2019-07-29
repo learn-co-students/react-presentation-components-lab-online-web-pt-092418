9454f8d4c93cf6b2994151d2d5b4644f87c6dfaf
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-07-29 10:44:26 -0400

    Interim checkin

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,23 @@
 // Code SimpleComponent Here
+import React, { Component} from 'react';
+
+export default class SimpleComponent extends Component {
+   constructor(props){
+        super(props)
+        this.state = {
+        mood: "happy"
+        };
+    };
+    handleClick= event => {
+        this.setState({ mood: this.state.mood === 'happy' ? 'sad': 'happy'})
+    }
+
+    render(){
+        return (
+            <div>
+                <button onClick={this.handleClick}>{this.state}</button>
+                
+            </div>
+        )
+    }
+}

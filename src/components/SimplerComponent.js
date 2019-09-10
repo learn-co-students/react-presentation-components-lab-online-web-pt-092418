a09a3240ccddea530f97f186f2e543de30df5bf3
github username <github email address>
2019-09-09 22:56:32 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 const mood = 'happy';
 
 const SimplerComponent = (props) => {
-  return <div onClick={props.handleClick}>I am just happy</div>
+  return <div onClick={props.handleClick}>I am just {mood}</div>
 }
 
 export default SimplerComponent;
